5aa6043ab1c6556cac1bf0fa7de84a1d8a0f40a1
Pedro Markun <pedro@markun.com.br>
2020-05-10 18:56:24 -0300

    static root

--- balsamo/settings.py
+++ balsamo/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR+'/static/'
 #All Auth
 
 LOGIN_REDIRECT_URL = '/add/person'
