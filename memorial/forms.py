f9571ef1d4cdd6c17396e821217069d1660feab0
Pedro Markun <pedro@markun.com.br>
2020-05-14 15:59:53 -0300

    first rc version

--- memorial/forms.py
+++ memorial/forms.py
@@ -1,12 +1,45 @@
 from django import forms
 from .models import Person, Memory
+import datetime
 
 class PersonForm(forms.ModelForm):
     class Meta:
         model = Person
-        exclude = []
+        exclude = ['user']
+
+    birth = forms.DateField(
+        widget=forms.DateInput(
+            format=('%d/%m/%Y',),
+            attrs={
+                "type" : "date",
+                "class": "",
+            }
+        ),
+        #initial=datetime.datetime.today,
+        # required=False,
+        # label=_("birth date"),
+    )
+
+    death = forms.DateField(
+        widget=forms.DateInput(
+            format='%d/%m/%Y',
+            attrs={
+                "type" : "date",
+                "class": "",
+            }
+        ),
+        initial=datetime.datetime.today,
+        input_formats='%d/%m/%Y',
+
+        # required=False,
+        # label=_("birth date"),
+    )
+
 
 class MemoryForm(forms.ModelForm):
     class Meta:
         model = Memory
-        exclude = []
+        exclude = ['user']
+        widgets = {
+            'person': forms.HiddenInput(),
+        }
--- memorial/views.py
+++ memorial/views.py
@@ -13,13 +13,17 @@ def page(request, page):
     return render(request, 'memorial/'+page+'.html')
 
 def add_person(request):
+    if request.user.is_anonymous:
+        return HttpResponseRedirect(reverse('account_signup'))
    # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = PersonForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            person = form.save()
+            person = form.save(commit=False)
+            person.user = request.user
+            person.save()
             return HttpResponseRedirect(reverse(view_person, args=(person.slug,)))
 
     # if a GET (or any other method) we'll create a blank form
@@ -38,7 +42,9 @@ def view_person(request, person):
         form = MemoryForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            memory = form.save()
+            memory = form.save(commit=False)
+            memory.user = request.user
+            memory.save()
             return HttpResponseRedirect(reverse(view_person, args=(p.slug,)))
 
     # if a GET (or any other method) we'll create a blank form
